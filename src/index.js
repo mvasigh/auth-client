5abf8515de6a5e2d8d548d4c321249239aa6de49
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-09-02 08:49:41 -0500

    add authReducer and sign up component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
+import SignUp from './components/auth/SignUp';
 
 ReactDOM.render(
-  <Router>
-    <App>
-      <Route exact path="/" component={Welcome} />
-    </App>
-  </Router>,
+  <Provider store={createStore(reducers, {})}>
+    <Router>
+      <App>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/signup" component={SignUp} />
+      </App>
+    </Router>
+  </Provider>,
   document.querySelector('#root')
 );
