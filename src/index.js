13e460c3a8e5e070eb305122946b6f88111f7380
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-09-02 08:34:27 -0500

    scaffold app and add basic routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+import Welcome from './components/Welcome';
+
+ReactDOM.render(
+  <Router>
+    <App>
+      <Route exact path="/" component={Welcome} />
+    </App>
+  </Router>,
+  document.querySelector('#root')
+);
